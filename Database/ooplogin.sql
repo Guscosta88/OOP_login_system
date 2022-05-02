a3f387b6c356ee0ccc584abf5989ff9bce08cbd9
Guscosta88 <guslfcosta@gmail.com>
2022-05-02 16:17:43 +1000

    database functionality added

---
+++ Database/ooplogin.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ooplogin;
+
+CREATE TABLE users (
+	users_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+    users_uid TINYTEXT not null,
+    users_pwd LONGTEXT not null,
+    users_email TINYTEXT not null
+);
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -8,9 +8,12 @@ if(isset($_POST["submit"]))
     $email = $_POST["email"];
 
 
+    include "../classes/dbh.classes.php";
     include "../classes/signup.classes.php";
     include "../classes/signup-contr.classes.php";
     $signup = new SignupContr($uid, $pwd, $pwdRepeat, $email);
 
+    $signup->signupUser();
 
+    header("location: ../index.php?error=none");
 }
